8092f655a01f1c0949acb67b9ee21d734ab65cca
Manish Dait <daitmanish88@gmail.com>
2023-04-28 21:28:02 +0530

    added gunicorn run

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #usr/bin/sh
 pip install -r requirement.txt
 pip install Flask
+pip install gunicorn
 pip install --upgrade --no-cache-dir git+https://github.com/StreamAlpha/tvdatafeed.git
-python3 app.py
+gunicorn app:app
